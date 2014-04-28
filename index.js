599e58a028ba5730c67cba27fb653b928aadf610
Mariusz Nowak <mariusz@medikoo.com>
2014-04-28 17:21:05 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 'use strict';
 
-var name, now, round, base, dateNow;
+module.exports = (function () {
+	var name, round, base, dateNow, nodeMicrotime;
 
-if ((typeof performance !== 'undefined') && performance) {
-	if (typeof performance.now === 'function') name = 'now';
-	else if (typeof performance.mozNow === 'function') name = 'mozNow';
-	else if (typeof performance.msNow === 'function') name = 'msNow';
-	else if (typeof performance.oNow === 'function') name = 'oNow';
-	else if (typeof performance.webkitNow === 'function') name = 'webkitNow';
+	if ((typeof performance !== 'undefined') && performance) {
+		if (typeof performance.now === 'function') name = 'now';
+		else if (typeof performance.mozNow === 'function') name = 'mozNow';
+		else if (typeof performance.msNow === 'function') name = 'msNow';
+		else if (typeof performance.oNow === 'function') name = 'oNow';
+		else if (typeof performance.webkitNow === 'function') name = 'webkitNow';
 
-	if (name) {
-		round = Math.round;
-		if (performance.timing &&
-				(typeof performance.timing.navigationStart === 'number')) {
-			base = performance.timing.navigationStart;
-		} else {
-			base = Date.now();
+		if (name) {
+			round = Math.round;
+			if (performance.timing &&
+					(typeof performance.timing.navigationStart === 'number')) {
+				base = performance.timing.navigationStart;
+			} else {
+				base = Date.now();
+			}
+			return function () { return round((base + performance[name]()) * 1000); };
 		}
-		now = function () {
-			return round((base + performance[name]()) * 1000);
-		};
 	}
-}
 
-if (!now && (typeof process !== 'undefined') && process) {
-	try { now = (require)('microtime').now; } catch (e) {} //jslint: skip
-}
+	if ((typeof process === 'object') && process) {
+		try { nodeMicrotime = (require)('microtime'); } catch (ignore) {} //jslint: ignore
+		if (nodeMicrotime) return nodeMicrotime.now;
+	}
 
-if (!now) {
 	dateNow = Date.now;
-	now = function () { return dateNow() * 1000; };
-}
-
-module.exports = now;
+	return function () { return dateNow() * 1000; };
+}());
